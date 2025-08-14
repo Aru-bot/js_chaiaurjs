b2852df767f0ac71312cc2aa42ccf43ce2833786
Arundhati Singh <arundhatisingh0101@gmail.com>
2025-08-14 08:02:53 +0000

    Describe your changes

---
+++ 03_basics/func.js
@@ -0,0 +1,13 @@
+const obj={name:"Maau"}
+function handleobj(obj){
+    console.log(`Hi ${obj.name}`)
+}
+handleobj(obj)
+handleobj({a_kauchi:"bilei"})
+
+const newarr=[23,32,454,4,2]
+
+function returnarr(newarr){
+    return newarr[3]
+}
+console.log(returnarr(newarr))
